23fce4b70bab6fb7cd67151346240586aa4241df
crunchypak <107717711+crunchypak@users.noreply.github.com>
2022-10-23 11:21:01 -0500

    selection sort

--- Sort.java
+++ Sort.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 /**
  * This class contains the selection, insertion, and merge sorting algorithms.
  *
@@ -10,19 +11,25 @@ public class Sort
     public Sort(int[] arr) {
         this.arr = arr;
     }
+    /**
+     * This method sorts the contents of an integer array using selection sort.
+     */
     public void selectionSort() {
-         int startIdx = 0;
-         int endIdx = arr.length - 1;
-         int minIdx;
-         while(startIdx != endIdx) {
-             minIdx = startIdx;
-             for(int i = startIdx; i < arr.length; i++) {
-                 if(arr[minIdx] > arr[i]) {
-                     minIdx = i;
-                 }
-             }
-             swap(minIdx, startIdx);
-             startIdx++;
+        int startIdx = 0;
+        int endIdx = arr.length - 1;
+        int minIdx;
+        while(startIdx != endIdx) {
+            //find smallest number in the window
+            minIdx = startIdx;
+            for(int i = startIdx; i < arr.length; i++) {
+                if(arr[i] < arr[minIdx]) {
+                    minIdx = i;
+                }
+            }
+            //swap smallest number with first number in the window
+            swap(minIdx, startIdx);
+            //shift start of window
+            startIdx++;
         }
     }
     
@@ -41,7 +48,14 @@ public class Sort
     }
     
     public static void main(String[] args) {
-        int[] arr = {24, 21, 0, 99};
+        final int N = 10;
+        int[] arr = new int[N];
+        Random rand = new Random();
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(90) + 10;
+        }
+        //int[] arr = {42, 31, 22, 20, 10};
+        
         Sort a = new Sort(arr);
         System.out.println(a);
         a.selectionSort();
