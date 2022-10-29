40711a74285ce49569dc18dfd29df4c824155cf8
crunchypak <107717711+crunchypak@users.noreply.github.com>
2022-10-29 17:41:46 -0500

    insertion sort

--- Sort.java
+++ Sort.java
@@ -11,6 +11,7 @@ public class Sort
     public Sort(int[] arr) {
         this.arr = arr;
     }
+
     /**
      * This method sorts the contents of an integer array using selection sort.
      */
@@ -32,13 +33,32 @@ public class Sort
             startIdx++;
         }
     }
-    
+
+    /**
+     * This method sorts the contents of an integer array using insertion sort.
+     */
+    public void insertionSort() {
+        int temp;
+        int j;
+        //start checking from second item in the array to the end
+        for(int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+            //keep comparing as long as temp is less than the value to the left of it
+            for(j = i - 1; j >= 0 && temp < arr[j]; j--) {
+                //shift the value to the right
+                arr[j + 1] = arr[j];
+            }
+            //insert the temp in the correct position
+            arr[j+1] = temp;
+        }
+    }
+
     private void swap(int idx1, int idx2) {
         int temp = arr[idx1];
         arr[idx1] = arr[idx2];
         arr[idx2] = temp;
     }
-    
+
     public String toString() {
         String arrayStr = "";
         for(int num : arr) {
@@ -46,19 +66,21 @@ public class Sort
         }
         return arrayStr;
     }
-    
+
     public static void main(String[] args) {
         final int N = 10;
         int[] arr = new int[N];
         Random rand = new Random();
         for(int i = 0; i < arr.length; i++) {
-            arr[i] = rand.nextInt(90) + 10;
+        arr[i] = rand.nextInt(90) + 10;
         }
-        //int[] arr = {42, 31, 22, 20, 10};
-        
+         
+        //int[] arr = {7, 5, 4, 3};
+
         Sort a = new Sort(arr);
         System.out.println(a);
-        a.selectionSort();
+        //a.selectionSort();
+        a.insertionSort();
         System.out.println(a);
     }
 }
